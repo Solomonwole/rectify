15016292531dbbe11349a94e023c1b097f7a8c55
SolomonWole <solomonwolemail@gmail.com>
2023-03-12 16:47:57 +0100

    Update

--- src/pages/redirect/Redirect.jsx
+++ src/pages/redirect/Redirect.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { LayoutPage } from '../../layout/LayoutPage';
 import { Error } from './styled';
+import errorpage from '../../assets/errorpage.gif';
 
 function RedirectPage() {
   return (
     <LayoutPage>
       <Error>
-        <lottie-player
-          src="https://assets2.lottiefiles.com/packages/lf20_q2pevjuc.json"
-          background="transparent"
-          speed="1"
-          style={{ width: '400px', height: '400px' }}
-          loop
-          autoplay
-        ></lottie-player>
+        <img src={errorpage} alt="" />
         <Link to="/">
           <button>Go Home</button>
         </Link>
